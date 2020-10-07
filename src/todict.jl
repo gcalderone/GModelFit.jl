c896df6891e963951a4c115d9a9c99da7e897849
Giorgio Calderone <giorgio.calderone@gmail.com>
2020-10-07 15:46:19 +0200

    Remove leading underscores in JSON names

--- src/todict.jl
+++ src/todict.jl
@@ -1,11 +1,11 @@
 function todict(param::Parameter)
     out = MDict()
-    out[:__meta] = param.meta
-    out[:__fixed] = param.fixed
-    out[:__value] = param.val
-    out[:__low] = param.low
-    out[:__high] = param.high
-    out[:__error] = !(param.low <= param.val <= param.high)
+    out[:meta] = param.meta
+    out[:fixed] = param.fixed
+    out[:value] = param.val
+    out[:low] = param.low
+    out[:high] = param.high
+    out[:error] = !(param.low <= param.val <= param.high)
     return out
 end
 
@@ -16,15 +16,15 @@ function todict(comp::AbstractComponent)
     ctype = split(string(typeof(comp)), ".")
     (ctype[1] == "GFit")  &&   (ctype = ctype[2:end])
     ctype = join(ctype, ".")
-    out[:__type] = ctype
+    out[:type] = ctype
 
-    out[:__params] = MDict()
+    out[:params] = MDict()
     for (pname, param) in getparams(comp)
         parname = pname[1]
         if pname[2] >= 1
-            parname = Symbol(parname, "[:__ * string(pname[2]) * ]")
+            parname = Symbol(parname, "[: * string(pname[2]) * ]")
         end
-        out[:__params][parname] = todict(param)
+        out[:params][parname] = todict(param)
     end
     return out
 end
@@ -35,12 +35,12 @@ function todict(ceval::CompEval)
     i = findall(isfinite.(y))
 
     out = MDict()
-    out[:__counter] = ceval.counter
-    out[:__min] = minimum(y[i])
-    out[:__max] = maximum(y[i])
-    out[:__mean] = mean(y[i])
-    out[:__error] = (length(i) == length(y))
-    out[:__y] = y
+    out[:counter] = ceval.counter
+    out[:min] = minimum(y[i])
+    out[:max] = maximum(y[i])
+    out[:mean] = mean(y[i])
+    out[:error] = (length(i) == length(y))
+    out[:y] = y
     return out
 end
 
@@ -50,49 +50,49 @@ function todict(reval::ReducerEval)
     i = findall(isfinite.(y))
 
     out = MDict()
-    out[:__counter] = reval.counter
-    out[:__min] = minimum(y[i])
-    out[:__max] = maximum(y[i])
-    out[:__mean] = mean(y[i])
-    out[:__error] = (length(i) == length(y))
-    out[:__y] = y
+    out[:counter] = reval.counter
+    out[:min] = minimum(y[i])
+    out[:max] = maximum(y[i])
+    out[:mean] = mean(y[i])
+    out[:error] = (length(i) == length(y))
+    out[:y] = y
     return out
 end
 
 
 function todict(pred::Prediction)
     out = MDict()
-    out[:__meta] = pred.meta
-    out[:__x] = domain(pred)
-    out[:__components] = MDict()
+    out[:meta] = pred.meta
+    out[:x] = domain(pred)
+    out[:components] = MDict()
     for (cname, ceval) in pred.cevals
-        out[:__components][cname] = todict(ceval)
+        out[:components][cname] = todict(ceval)
     end
-    out[:__reducers] = MDict()
+    out[:reducers] = MDict()
     for (rname, reval) in pred.revals
-        out[:__reducers][rname] = todict(reval)
+        out[:reducers][rname] = todict(reval)
     end
-    out[:__main_reducer] = pred.rsel
+    out[:main_reducer] = pred.rsel
     return out
 end
 
 
 function todict(pred::Prediction, data::Measures_1D)
     out = MDict()
-    out[:__meta] = data.meta
-    out[:__y] = data.val
-    out[:__err] = data.unc
-    out[:__residuals] = (data.val .- geteval(pred)) ./ data.unc
+    out[:meta] = data.meta
+    out[:y] = data.val
+    out[:err] = data.unc
+    out[:residuals] = (data.val .- geteval(pred)) ./ data.unc
     return out
 end
 
 
 function todict(param::BestFitPar)
     out = MDict()
-    out[:__val] = param.val
-    out[:__unc] = param.unc
-    out[:__fixed] = param.fixed
-    out[:__patched] = param.patched
+    out[:val] = param.val
+    out[:unc] = param.unc
+    out[:fixed] = param.fixed
+    out[:patched] = param.patched
     return out
 end
 
@@ -107,16 +107,16 @@ end
 
 function todict(res::BestFitResult)
     out = MDict()
-    out[:__components] = MDict()
+    out[:components] = MDict()
     for (cname, comp) in res.comps
-        out[:__components][cname] = todict(comp)
+        out[:components][cname] = todict(comp)
     end
-    out[:__ndata] = res.ndata
-    out[:__dof] = res.dof
-    out[:__cost] = res.cost
-    out[:__status] = res.status
-    out[:__log10testprob] = res.log10testprob
-    out[:__elapsed] = res.elapsed
+    out[:ndata] = res.ndata
+    out[:dof] = res.dof
+    out[:cost] = res.cost
+    out[:status] = res.status
+    out[:log10testprob] = res.log10testprob
+    out[:elapsed] = res.elapsed
     return out
 end
 
@@ -130,34 +130,34 @@ function todict(model::Model,
 
     out = MDict()
 
-    out[:__components] = MDict()
+    out[:components] = MDict()
     for (cname, comp) in model.comps
-        out[:__components][cname] = todict(comp)
-        out[:__components][cname][:__fixed] = model.cfixed[cname]
-        out[:__components][cname][:__meta] = metadict(model, cname)
+        out[:components][cname] = todict(comp)
+        out[:components][cname][:fixed] = model.cfixed[cname]
+        out[:components][cname][:meta] = metadict(model, cname)
     end
 
-    out[:__predictions] = Vector{MDict}()
+    out[:predictions] = Vector{MDict}()
     for pred in model.preds
-        push!(out[:__predictions], todict(pred))
+        push!(out[:predictions], todict(pred))
         for (name, ceval) in pred.cevals
-            out[:__predictions][end][:__components][name][:__meta] = metadict(model, name)
+            out[:predictions][end][:components][name][:meta] = metadict(model, name)
         end
         for (name, ceval) in pred.revals
-            out[:__predictions][end][:__reducers][name][:__meta] = metadict(model, name)
+            out[:predictions][end][:reducers][name][:meta] = metadict(model, name)
         end
     end
 
     if !isnothing(data)
-        out[:__data] = Vector{MDict}()
+        out[:data] = Vector{MDict}()
         @assert length(model.preds) == length(data)
         for i in 1:length(data)
-            push!(out[:__data], todict(model.preds[i], data[i]))
+            push!(out[:data], todict(model.preds[i], data[i]))
         end
     end
 
     if !isnothing(bestfit)
-        out[:__bestfit] = todict(bestfit)
+        out[:bestfit] = todict(bestfit)
     end
 
     return out
