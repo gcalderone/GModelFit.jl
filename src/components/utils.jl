b878dc1ec5694d69f4783b453c3551440c1f63c8
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-04-21 17:01:47 +0200

    Added check for non-finite values

--- src/components/utils.jl
+++ src/components/utils.jl
@@ -3,6 +3,8 @@ using Dierckx, QuadGK
 
 function int_tabulated(λ, f; int_k=3)
     @assert issorted(λ)
+    @assert all(isfinite.(λ))
+    @assert all(isfinite.(f))
     itp = Spline1D(λ, f, k=int_k, bc="error")
     return quadgk(itp, λ[1], λ[end])
 end
