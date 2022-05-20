d2b3aff629583acde9b14627ed0e00a380bd39d8
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-05-21 00:48:01 +0200

    Removed file

--- src/utils.jl
+++
@@ -1,9 +0,0 @@
-
-function int_tabulated(x, y)
-    @assert issorted(x)
-    @assert all(isfinite.(x))
-    @assert all(isfinite.(y))
-    b = x[2:end] .- x[1:end-1]
-    h = y[2:end] .+ y[1:end-1]
-    return sum(b .* h) / 2
-end
