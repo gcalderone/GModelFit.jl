219650dceb7e551ada1caddf698461800bbf14eb
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-05-23 12:23:59 +0200

    Minor changes

--- src/HashVector.jl
+++ src/HashVector.jl
@@ -20,40 +20,43 @@ struct HashVector{V}
         new{V}(OrderedDict{Symbol, Int}(), data)
 end
 
-length(hv::HashVector) = length(getfield(hv, :dict))
-propertynames(hv::HashVector) = keys(getfield(hv, :dict))
+internal_dict(hv::HashVector{V}) where V = getfield(hv, :dict)
+internal_data(hv::HashVector{V}) where V = getfield(hv, :data)
 
-internal_vector(hv::HashVector{V}) where V = getfield(hv, :data)
-values(hv::HashVector{V}) where V = getfield(hv, :data)[collect(values(getfield(hv, :dict)))]
+
+length(hv::HashVector) = length(internal_dict(hv))
+propertynames(hv::HashVector) = keys(internal_dict(hv))
+
+values(hv::HashVector{V}) where V = internal_data(hv)[collect(values(internal_dict(hv)))]
 
 getindex(hv::HashVector{V}, key::Symbol) where V = getproperty(hv, key)
 function getproperty(hv::HashVector{V}, key::Symbol) where V
-    id = getfield(hv, :dict)[key]
-    return getfield(hv, :data)[id]
+    id = internal_dict(hv)[key]
+    return internal_data(hv)[id]
 end
 
 setindex!(hv::HashVector{V}, value::V, key::Symbol) where V = setproperty!(hv, key, value)
 function setproperty!(hv::HashVector{V}, key::Symbol, value::V) where V
-    if haskey(getfield(hv, :dict), key)
-        id = getfield(hv, :dict)[key]
-        getfield(hv, :data)[id] = value
+    if haskey(internal_dict(hv), key)
+        id = internal_dict(hv)[key]
+        internal_data(hv)[id] = value
     else
-        id = length(getfield(hv, :data)) + 1
-        getfield(hv, :dict)[key] = id
-        push!(getfield(hv, :data), value)
+        id = length(internal_data(hv)) + 1
+        internal_dict(hv)[key] = id
+        push!(internal_data(hv), value)
     end
     return value
 end
 
 function iterate(hv::HashVector{V}, state...) where V
-    out = iterate(getfield(hv, :dict), state...)
+    out = iterate(internal_dict(hv), state...)
     isnothing(out)  &&  (return nothing)
     return (out[1][1] => getproperty(hv, out[1][1]), out[2])
 end
 
 
 function empty!(hv::HashVector{V}) where V
-    empty!(getfield(hv, :dict))
-    empty!(getfield(hv, :data))
+    empty!(internal_dict(hv))
+    empty!(internal_data(hv))
     nothing
 end
