6ae9c6398492766597d230b39f1ac6e47feb6398
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-06-07 23:01:49 +0200

    Implemented MultiFitData

--- src/fit.jl
+++ src/fit.jl
@@ -34,122 +34,30 @@ function fit!(mzer::AbstractMinimizer, model::Model, data::Measures)
 end
 
 
-function fit!(multi::MultiModel, data::Vector{Measures{N}};
-              minimizer=lsqfit(), dry=false) where N
-    timestamp = now()
-    evaluate(multi)
+fit!(multi::MultiModel, data::Vector{Measures}) =
+    fit!(lsqfit(), multi, data)
 
-    data1d = [flatten(data[id]) for id in 1:length(multi)]
-    resid1d = fill(NaN, sum(length.(data1d)))
-    params = Vector{Parameter}()
-    for id in 1:length(multi)
-        append!(params, multi[id].meval.params[multi[id].meval.ifree])
-    end
-    @assert length(params) > 0 "No free parameter in the model"
-
-    function private_func(pvalues::Vector{Float64})
-        i1 = 1
-        for id in 1:length(multi)
-            nn = length(multi[id].meval.ifree)
-            i2 = i1 + nn - 1
-            multi[id].meval.pvalues[multi[id].meval.ifree] = pvalues[i1:i2]
-            i1 += nn
-        end
-        eval_step2(multi)
-        eval_step3(multi)
-
-        i1 = 1
-        for id in 1:length(multi)
-            nn = length(data1d[id])
-            i2 = i1 + nn - 1
-            resid1d[i1:i2] .= (multi[id]() .- data1d[id].val) ./ data1d[id].unc
-            i1 += nn
-        end
-        evaluate_showvalues(x) = () -> begin
-            dof = (length(resid1d) - length(pvalues))
-            [(:fit_stat, sum(abs2, x) / dof)]
-        end
-        ProgressMeter.next!(prog; showvalues=evaluate_showvalues(resid1d))
-        return resid1d
-    end
-
-    dof = length(resid1d) - length(params)
-    prog = ProgressUnknown("Model (dof=$dof) evaluations:", dt=0.5, showspeed=true)
-    if !dry
-        result = minimize(minimizer, private_func, params)
-        private_func(result.best)
-        i1 = 1
-        for id in 1:length(multi)
-            nn = length(multi[id].meval.ifree)
-            i2 = i1 + nn - 1
-            eval_step4(multi[id], result.unc[i1:i2])
-            i1 += nn
-        end
-    else
-        private_func(getfield.(params, :val))
-        i1 = 1
-        for id in 1:length(multi)
-            nn = length(multi[id].meval.ifree)
-            i2 = i1 + nn - 1
-            eval_step4(multi[id], fill(NaN, i2-i1+1))
-            i1 += nn
-        end
-    end
-    ProgressMeter.finish!(prog)
-
-    # Prepare output
-    ndata = length(resid1d)
-    nfree = length(params)
-    dof = ndata - nfree
-    fitstat = sum(abs2, resid1d)
-    gofstat = sum(abs2, resid1d)
-    elapsed = now() - timestamp
-    @assert isa(elapsed, Millisecond)
-    tp = NaN
-    try; tp = logccdf(Chisq(dof), gofstat) * log10(exp(1)); catch; end
-    return FitResult(timestamp, elapsed.value / 1.e3,
-                     ndata, nfree, dof, fitstat, gofstat,
-                     tp,
-                     resid1d, (dry  ?  nothing  :  result))
-end
-
-
-
-function free_param_names(model::Model)
-    out = Vector{String}()
-    for (cname, ceval) in model.cevals
-        for (pname, par) in ceval.params
-            if (!par.fixed)  &&  (model.cevals[cname].cfixed == 0)
-                push!(out, "[:$(cname)].$(pname)")
-            end
-        end
-    end
-    return out
-end
-
-function free_param_names(multi::MultiModel)
-    out = Vector{String}()
-    for id in 1:length(multi)
-        append!(out, "[$(string(id))]" .* free_param_names(multi[id]))
-    end
-    return out
+function fit!(mzer::AbstractMinimizer, multi::MultiModel, data::Vector{Measures{N}}) where N
+    fd = MultiFitData(model, data)
+    status = fit!(mzer, fd)
+    return FitResult(fd, status)
 end
 
 
 function print_param_covariance(model::Union{Model, MultiModel}, fitres::FitResult;
                                 param::Union{Nothing, String}=nothing, sort=false, thresh=0.)
-    @assert isa(fitres.mzer.specific, CMPFit.Result)
+    @assert isa(fitres.mzer.internal, CMPFit.Result)
     if isa(model, Model)
         @assert isnothing(model.parent)
     end
     names = free_param_names(model)
-    @assert length(names)^2 == length(fitres.mzer.specific.covar)
+    @assert length(names)^2 == length(fitres.mzer.internal.covar)
     ii = Vector{Int}()
     jj = Vector{Int}()
     covar = Vector{Float64}()
     for i in 1:length(names)
         for j in i+1:length(names)
-            push!(covar, fitres.mzer.specific.covar[i, j])
+            push!(covar, fitres.mzer.internal.covar[i, j])
             push!(ii, i)
             push!(jj, j)
         end
--- src/fitdata.jl
+++ src/fitdata.jl
@@ -1,19 +1,20 @@
 abstract type AbstractFitData end
 
+# ====================================================================
 struct FitData <: AbstractFitData
     timestamp::DateTime
+    progress::ProgressUnknown
     model::Model
     data::Vector{Float64}
     unc::Vector{Float64}
     resid::Vector{Float64}
     ifree::Vector{Int}
     dof::Int
-    progress::ProgressUnknown
 
     function FitData(model::Model, data::Measures{N}) where N
         evaluate(model)
         data1d = flatten(data)
-        resid = (model() .- data1d.val) ./ data1d.unc
+        resid = fill(NaN, length.(data1d))
         ifree = Vector{Int}()
         i = 1
         for (cname, hv) in model.params
@@ -24,11 +25,11 @@ struct FitData <: AbstractFitData
                 i += 1
             end
         end
-        @assert length(ifree) > 0 "No free parameter in the model"
-
-        dof = length(resid) - length(ifree)
+        nfree = length(ifree)
+        @assert nfree > 0 "No free parameter in the model"
+        dof = length(resid) - nfree
         prog = ProgressUnknown("Model (dof=$dof) evaluations:", dt=0.5, showspeed=true)
-        return new(now(), model, data1d.val, data1d.unc, resid, ifree, dof, prog)
+        return new(now(), prog, model, data1d.val, data1d.unc, resid, ifree, dof)
     end
 end
 
@@ -62,4 +63,90 @@ end
 
 
 
-# TODO: MultiFitData
+# ====================================================================
+struct MultiFitData <: AbstractFitData
+    timestamp::DateTime
+    progress::ProgressUnknown
+    multi::MultiModel
+    fds::Vector{FitData}
+    resid::Vector{Float64}
+    dof::Int
+
+    function MultiFitData(multi::MultiModel, datasets::Vector{Measures{N}}) where N
+        @assert length(multi) == length(datasets)
+        evaluate(multi)
+        fds = [FitData(multi[id], datasets[id]) for id in 1:length(multi)]
+        resid = fill(NaN, sum(length.(getfield.(fds, :data))))
+        nfree = sum(length.(getfield.(fds, :ifree)))
+        @assert nfree > 0 "No free parameter in the model"
+        dof = length(resid) - nfree
+        prog = ProgressUnknown("Model (dof=$dof) evaluations:", dt=0.5, showspeed=true)
+        return new(now(), prog, multi, fds, resid, dof)
+    end
+end
+
+function free_params(fd::MultiFitData)
+    out = Vector{Parameter}()
+    for id in 1:length(fd.multi)
+        append!(out, free_params(fd.fds[id]))
+    end
+    return out
+end
+residuals(fd::MultiFitData) = fd.resid
+
+function evaluate!(fd::MultiFitData, pvalues::Vector{Float64})
+    # We need to copy all parameter values before evaluate!, to ensure
+    # all patch functions use the current parameter values
+    i1 = 1
+    for id in 1:length(fd.multi)
+        nn = length(fd.fds[id].ifree)
+        if nn > 0
+            i2 = i1 + nn - 1
+            internal_data(fd.fds[id].model.pvalues)[fd.fds[id].ifree] .= pvalues[i1:i2]
+            i1 += nn
+        end
+    end
+    # Now proceed to evaluation
+    i1 = 1
+    for id in 1:length(fd.multi)
+        nn = length(fd.fds[id].ifree)
+        if nn > 0
+            i2 = i1 + nn - 1
+            evaluate!(fd.fds[id], pvalues[i1:i2])
+            i1 += nn
+        end
+    end
+    # Populate resid vector
+    i1 = 1
+    for id in 1:length(fd.multi)
+        nn = length(fd.fds[id].resid)
+        if nn > 0
+            i2 = i1 + nn - 1
+            fd.resid[i1:i2] .= fd.fds[id].resid
+            i1 += nn
+        end
+    end
+    return fd.resid
+end
+
+
+function finalize!(fd::MultiFitData, best::Vector{Float64}, unc::Vector{Float64})
+    i1 = 1
+    for id in 1:length(fd.multi)
+        nn = length(fd.fds[id].ifree)
+        if nn > 0
+            i2 = i1 + nn - 1
+            finalize!(fd.fds[id], best[i1:i2], unc[i1:i2])
+            i1 += nn
+        end
+    end
+    ProgressMeter.finish!(fd.progress)
+end
+
+
+function error!(fd::MultiFitData)
+    for id in 1:length(fd.multi)
+        finalize!(fd.fds[id])
+    end
+    ProgressMeter.finish!(fd.progress)
+end
