970a000a8682af00b755fcf0c35430492fc29089
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-06-07 16:49:41 +0200

    Minor changes

--- src/multimodel.jl
+++ src/multimodel.jl
@@ -3,12 +3,9 @@
 #
 mutable struct MultiModel <: AbstractMultiModel
     models::Vector{Model}
-    patchfuncts::Vector{λFunct}
-    patchcomps::Vector{OrderedDict{Symbol, HashVector{Float64}}}
 
     function MultiModel(v::Vararg{Model})
-        multi = new([v...], Vector{λFunct}(),
-                    Vector{OrderedDict{Symbol, HashVector{Float64}}}())
+        multi = new([v...])
         for m in v
             m.parent = multi
         end
@@ -19,6 +16,12 @@ end
 
 Base.getindex(m::MultiModel, id::Int) = m.models[id]
 Base.length(m::MultiModel) = length(m.models)
+
+function push!(multi::MultiModel, model::Model)
+    push!(multi.models, model)
+    model.parent = multi
+    evaluate(multi)
+end
 
 
 function evaluate(multi::MultiModel)
@@ -58,17 +61,4 @@ function eval_step4(multi::MultiModel)
     for id in 1:length(multi.models)
         eval_step4(multi.models[id])
     end
-end
-
-function push!(multi::MultiModel, model::Model)
-    push!(multi.models, model)
-    model.parent = multi
-    evaluate(multi)
-end
-
-function patch!(func::λFunct, multi::MultiModel)
-    evaluate(multi)  # ensure sub models are evaluated before adding new patch functions
-    push!(multi.patchfuncts, func)
-    evaluate(multi)
-    return multi
 end
