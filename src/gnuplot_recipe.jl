677556352fb7b42f6b2df68cb732c247a87dac46
Giorgio Calderone <giorgio.calderone@gmail.com>
2024-02-13 13:38:29 +0100

    Added keep= and skip= keywords to Gnuplot recipe for ModelSnapshot

--- src/gnuplot_recipe.jl
+++ src/gnuplot_recipe.jl
@@ -8,14 +8,18 @@ Gnuplot.recipe(data::Measures{1}) =
 
 Gnuplot.recipe(model::Model) = Gnuplot.recipe(GModelFit.ModelSnapshot(model))
 
-function Gnuplot.recipe(model::GModelFit.ModelSnapshot)
+function Gnuplot.recipe(model::GModelFit.ModelSnapshot;
+                        keep=Symbol[], skip=Symbol[])
     @assert ndims(domain(model)) == 1
     out = Vector{Gnuplot.AbstractGPSpec}()
     for (k,v) in model.buffers
         (k == model.maincomp)  &&  continue
-        #isa(v.comp, GModelFit.FComp)  ||  isa(v.comp, GModelFit.SumReducer)  ||  continue
-        append!(out, Gnuplot.parseSpecs(coords(domain(model)), model(k),
-                                        "with lines t '$(k)'"))
+        (k in skip)  &&  continue
+        if (length(keep) == 0)  ||  (k in keep)
+            #isa(v.comp, GModelFit.FComp)  ||  isa(v.comp, GModelFit.SumReducer)  ||  continue
+            append!(out, Gnuplot.parseSpecs(coords(domain(model)), model(k),
+                                            "with lines t '$(k)'"))
+        end
     end
     append!(out, Gnuplot.parseSpecs(coords(domain(model)), model(),
                                     "with lines t 'Model' lc rgb 'black' lw 2"))
