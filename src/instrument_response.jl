edcfeff6c4d97b036ca2ddf293bccbc667d26ac5
Giorgio Calderone <giorgio.calderone@gmail.com>
2025-07-16 18:39:07 +0200

    Minor changes

--- src/instrument_response.jl
+++ src/instrument_response.jl
@@ -6,29 +6,29 @@ abstract type AbstractInstrumentResponse end
 
 struct IREval{T <: AbstractInstrumentResponse, TDomain <: AbstractDomain}
     IR::T
-    data_domain::TDomain
-    model_domain::AbstractDomain
+    unfolded_domain::AbstractDomain
+    folded_domain::TDomain
     folded::Vector{Float64}
     folded_ad::Vector{Union{Dual, Float64}}
 
-    function IREval(IR::T, data_domain::TDomain) where {T <: AbstractInstrumentResponse, TDomain <: AbstractDomain}
-        mdomain = model_domain(IR, data_domain)
-        return new{T, TDomain}(IR, data_domain, mdomain, 
-                               Vector{Float64}(undef, length(data_domain)),
-                               Vector{Union{Dual, Float64}}(undef, length(data_domain)))
+    function IREval(IR::T, folded_domain::TDomain) where {T <: AbstractInstrumentResponse, TDomain <: AbstractDomain}
+        d = model_domain(IR, folded_domain)
+        return new{T, TDomain}(IR, d, folded_domain,
+                               Vector{Float64}(undef, length(folded_domain)),
+                               Vector{Union{Dual, Float64}}(undef, length(folded_domain)))
     end
 end
 
 apply_ir!(ireval::IREval, unfolded::Vector{Float64}) =
-    apply_ir!(ireval.IR, ireval.data_domain, ireval.folded   , ireval.model_domain, unfolded)
+    apply_ir!(ireval.IR, ireval.folded_domain, ireval.folded   , ireval.unfolded_domain, unfolded)
 
 apply_ir!(ireval::IREval, unfolded::Vector) =
-    apply_ir!(ireval.IR, ireval.data_domain, ireval.folded_ad, ireval.model_domain, unfolded)
+    apply_ir!(ireval.IR, ireval.folded_domain, ireval.folded_ad, ireval.unfolded_domain, unfolded)
 
 
 function fold_model(ireval::IREval, unfolded::Vector{Float64})
     folded = deepcopy(ireval.folded)
-    apply_ir!(ireval.IR, ireval.data_domain, folded, ireval.model_domain, unfolded)
+    apply_ir!(ireval.IR, ireval.folded_domain, folded, ireval.unfolded_domain, unfolded)
     return folded
 end
 
@@ -39,7 +39,7 @@ last_eval_folded(ireval::IREval) = ireval.folded
 struct IdealInstrument <: AbstractInstrumentResponse
 end
 
-model_domain(IR::IdealInstrument, data_domain::AbstractDomain) = data_domain
+model_domain(IR::IdealInstrument, folded_domain::AbstractDomain) = folded_domain
 apply_ir!(IR::IdealInstrument,
-          data_domain::AbstractDomain, folded::Vector,
-          model_domain::AbstractDomain, unfolded::Vector) = folded .= unfolded
+          folded_domain::AbstractDomain  , folded::Vector,
+          unfolded_domain::AbstractDomain, unfolded::Vector) = folded .= unfolded
--- src/snapshot.jl
+++ src/snapshot.jl
@@ -26,7 +26,7 @@ function ModelSnapshot(meval::ModelEval, bestfit::PVModel{Parameter})
     ModelSnapshot(deepcopy(meval.domain), deepcopy(bestfit),
                   OrderedDict([Pair(cname, ceval.tpar.buffer) for (cname, ceval) in meval.cevals]),
                   meval.seq[end],
-                  meval.ireval.data_domain, meval.ireval.folded,
+                  meval.ireval.folded_domain, meval.ireval.folded,
                   comptypes(meval.model),
                   OrderedDict([Pair(cname, isfreezed(meval.model, cname)) for cname in keys(meval.cevals)]),
                   deps, evalcounters(meval))
